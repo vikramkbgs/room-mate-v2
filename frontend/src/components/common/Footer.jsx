a11b85d799d0447cfc53fe7265bc35a18d8d682a
vikramkbgs <vikramkbgs@gmail.com>
2024-02-19 13:03:07 +0530

    adding comman component for resuse

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <>
+      <footer className="navbar-dark bg-dark mt-4">
+        <div className="container p-4 text-white">
+          <div>Developed and Maintained by Vikram Kumar</div>
+          <a href="https://github.com/vikramkbgs">
+            <i className="bi bi-github"></i>
+          </a>
+        </div>
+      </footer>
+    </>
+  );
+};
---
+++ frontend/src/components/common/PlainNavBar.jsx
@@ -0,0 +1,20 @@
+import { Link, NavLink } from "react-router-dom";
+
+export const PlainNavBar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md sticky-top navbar-dark bg-dark shadow">
+        <div className="container-fluid">
+          <Link to={`/`} className=" navbar-brand fw-bold">
+            <img src="/images/branding.png" alt="Room Mate" id="nav-image" />
+          </Link>
+          <div className="navbar-nav justify-content-end">
+            <NavLink to={`/login`} className="nav-link">
+              Sign In
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
